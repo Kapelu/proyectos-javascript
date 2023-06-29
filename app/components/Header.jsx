0df4cec29ccd0ead38fd38b1701ffe47888e99a1
kapelu <ddaniel_calderon@hotmail.com>
2023-06-29 20:25:44 -0300

    Update 29-06-2023

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -1,5 +1,8 @@
 import Link from 'next/link'
 import '../components/header.css'
+
+
+
 export default function Header() {
 	return (
 		<header className='header'>
@@ -17,25 +20,6 @@ export default function Header() {
 						/>
 					</Link>
 				</div>
-				<button className='menu-btn'>
-					<svg
-						xmlns='http://www.w3.org/2000/svg'
-						width='24'
-						height='24'
-						viewBox='0 0 24 24'
-					>
-						<path d='M4 6H20V8H4zM4 11H20V13H4zM4 16H20V18H4z' />
-					</svg>
-					<svg
-						className='none'
-						xmlns='http://www.w3.org/2000/svg'
-						width='24'
-						height='24'
-						viewBox='0 0 24 24'
-					>
-						<path d='M16.192 6.344L11.949 10.586 7.707 6.344 6.293 7.758 10.535 12 6.293 16.242 7.707 17.656 11.949 13.414 16.192 17.656 17.606 16.242 13.364 12 17.606 7.758z' />
-					</svg>
-				</button>
 				<nav className='menu'>
 					<Link href='#'>Inicio</Link>
 					<Link href='#acerca'>Acerca</Link>
