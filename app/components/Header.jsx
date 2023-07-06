aa305c6e231f0bb5a499cb566ac96783b184d21d
kapelu <ddaniel_calderon@hotmail.com>
2023-07-06 00:47:41 -0300

    Update 06-06-2023

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -1,6 +1,7 @@
 import React, {useState} from 'react'
 import Link from 'next/link'
 import '../globals.css'
+import './header.css'
 
 export default function Header() {
 	const [isMenuOpen, setIsMenuOpen] = useState(false)
@@ -22,10 +23,18 @@ export default function Header() {
 					</Link>
 				</div>
 				<button
-					className='menu-btn'
-					onClick={() => setIsMenuOpen(!isMenuOpen)}
+					className={`menu-btn ${isMenuOpen ? 'menu-hamburguer' : ''}`}
+					onClick={() => {
+						const $btnMenu = document.querySelector('.menu-btn')
+
+						setIsMenuOpen(!isMenuOpen)
+
+						$btnMenu.firstElementChild.classList.toggle('menu-none')
+						$btnMenu.lastElementChild.classList.toggle('menu-none')
+					}}
 				>
 					<svg
+						className='menu-hamburguer'
 						xmlns='http://www.w3.org/2000/svg'
 						width='24'
 						height='24'
@@ -34,27 +43,16 @@ export default function Header() {
 						<path d='M4 6H20V8H4zM4 11H20V13H4zM4 16H20V18H4z' />
 					</svg>
 					<svg
-						class="none"
-						xmlns="http://www.w3.org/2000/svg"
-						width="24"
-						height="24"
-						viewBox="0 0 24 24"
-					>
-						<path
-							d="M16.192 6.344L11.949 10.586 7.707 6.344 6.293 7.758 10.535 12 6.293 16.242 7.707 17.656 11.949 13.414 16.192 17.656 17.606 16.242 13.364 12 17.606 7.758z"
-						/>
-					</svg>
-					{/* <svg
-						className='none'
+						class='menu-none'
 						xmlns='http://www.w3.org/2000/svg'
 						width='24'
 						height='24'
-						viewBox='0 0 14 10'
+						viewBox='0 0 24 24'
 					>
-						<path d='M5 1 1 5l4 4m6-8L7 5l4 4' />
-					</svg> */}
+						<path d='M16.192 6.344L11.949 10.586 7.707 6.344 6.293 7.758 10.535 12 6.293 16.242 7.707 17.656 11.949 13.414 16.192 17.656 17.606 16.242 13.364 12 17.606 7.758z' />
+					</svg>
 				</button>
-				<nav className='menu'>  {/* {`menu ${isMenuOpen ? 'menu-btn' : 'none'}`} */}
+				<nav className={`menu ${isMenuOpen ? 'is-active' : ''}`}>
 					<Link href='#'>Inicio</Link>
 					<Link href='#acerca'>Acerca</Link>
 					<Link href='#servicios'>Servicios</Link>
@@ -64,6 +62,5 @@ export default function Header() {
 				</nav>
 			</section>
 		</header>
-
 	)
 }
